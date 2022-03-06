5658486472e3a9952824e21873a5133c74c2e78f
jeffer <jeffersunde72@gmail.com>
2022-03-06 22:40:58 +0200

    Finished

--- Border-Radius-Previewer/scripts/script.js
+++ Border-Radius-Previewer/scripts/script.js
@@ -5,7 +5,6 @@ const inputs = document.querySelectorAll("input")
 inputs.forEach(element => {
     element.addEventListener("change", (event) => {
         const value = element.value
-        console.log(value, event.target)
         changeBorder(value, event.target)
     })
 })
@@ -13,15 +12,24 @@ inputs.forEach(element => {
 function changeBorder(value, event) {
     if (event.classList == "top-left") {
         border.style.borderTopLeftRadius = `${value}px`
+        addToCopy("leftTop", value, event.classList)
     } else if (event.classList == "top-right") {
         border.style.borderTopRightRadius = `${value}px`
+        addToCopy("topRight", value, event.classList)
     } else if (event.classList == "bottom-right") {
         border.style.borderBottomRightRadius = `${value}px`
+        addToCopy("bottomRight", value, event.classList)
     } else if (event.classList == "bottom-left") {
         border.style.borderBottomLeftRadius = `${value}px`
+        addToCopy("bottomLeft", value, event.classList)
     }
 }
 
-function addToCopy {
-    
+function addToCopy(outputParagraph, value, whatClass) {
+    var where = document.querySelector(`.${outputParagraph}`)
+
+    console.log(where)
+    console.log(whatClass)
+
+    where.innerText = `border-${whatClass}-radius: ${value}px;`
 }
