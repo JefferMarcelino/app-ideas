d44a0caac74b7a52c7cb21fcc6b750a73ff8f7b7
jeffer <jeffersunde72@gmail.com>
2022-03-09 05:03:24 +0200

    Multiplication done

---
+++ Calculator/scripts/script.js
@@ -0,0 +1,39 @@
+const output = document.querySelector(".output")
+const preview = document.querySelector("preview")
+const numberButtons = document.querySelectorAll("button.number")
+const operations = document.querySelectorAll("button.operations")
+let result = false
+
+
+numberButtons.forEach(element => {
+    element.addEventListener("click", event => {
+        console.log(element.textContent)
+        output.innerText += `${element.textContent}`
+    })
+})
+
+operations.forEach(element => {
+    element.addEventListener("click", () => {
+        const number = Number(output.textContent)
+        output.innerText = ``
+
+        if (element.textContent == "X") {    
+            multiplication(output, number, element)
+        }
+    })
+})
+
+function multiplication(saida, numero, elemento) {
+        console.log("Multiplicando")
+        if (result == false) {
+            result = 1
+            result *= numero
+        } else {
+            var otherNumbers = numero 
+            result *= otherNumbers
+        }
+}
+
+function show(result) {
+    console.log("result")
+}
