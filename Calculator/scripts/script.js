b6601d2ae33a042ad1e268b28e5eadde40f08b40
jeffer <jeffersunde72@gmail.com>
2022-03-20 19:59:15 +0200

    Calculator

--- Calculator/scripts/script.js
+++ Calculator/scripts/script.js
@@ -1,11 +1,42 @@
-const buttons = document.querySelectorAll("button")
+const numberButtons = document.querySelectorAll("button.number")
+const operationButtons = document.querySelectorAll("button.operation")
+
+const equalButton = document.querySelector(".equal")
+
+const clearButton = document.querySelector(".clear")
+
+const lastNumButton = document.querySelector(".last-num")
+
 const output = document.querySelector(".current-operand")
 
-buttons.forEach(button => {
+numberButtons.forEach(button => {
     button.addEventListener("click", event => {
-        if (button.classList.contains("number")) {
-            //output.innerHTML += button.value
-            console.log(button.value)
+        if (output.innerHTML === "0") {
+            output.innerText = button.textContent
+        } else {
+            output.innerText += button.textContent
+        }
+        console.log(button.textContent)
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener("click", event => {
+        if (output.innerText !== "") {
+            output.innerText += button.textContent
+            console.log(button.textContent)
         }
     })
+})
+
+equalButton.addEventListener("click", () => {
+    output.innerText = eval(output.innerHTML.replace("X", "*"))
+})
+
+clearButton.addEventListener("click", () => {
+    output.innerHTML = 0
+})
+
+lastNumButton.addEventListener("click", () => {
+    output.innerHTML = output.innerHTML.substring(0, output.innerHTML.length - 1)
 })
